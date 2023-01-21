224ca061bf82eed5f4e1544953a447aff75289cd
SutPi0154 <sutpi070@gmail.com>
2023-01-21 17:27:05 +0630

    update

--- 3-learn-js-basic/jsDomBom/js/area.js
+++ 3-learn-js-basic/jsDomBom/js/area.js
@@ -6,24 +6,21 @@ const breadth = document.getElementById(`breadth`);
 const calculate = document.getElementById(`calculate`);
 const records = document.getElementById(`records`);
 
-// calculate.onclick = () => {
-//   CalcArea = `${width.innerText}ft` * `${breadth.innerText}`;
-//   console.log(CalcArea);
-// };
 const clearResult = () => (result.innerHTML = null);
 
 const CalcArea = () => {
-  const area = width.valueAsNumber * breadth.valueAsNumber;
-  //   console.log(typeof area);
-  //   console.log(typeof width.valueAsNumber);
-  //   console.log(typeof breadth.valueAsNumber);
-  result.innerText = `width :${width.value}ft * breadth :${breadth.value}ft = ${area}sqft`;
-  width.value = breadth.value = null;
+  if (width.valueAsNumber && breadth.valueAsNumber) {
+    const area = width.valueAsNumber * breadth.valueAsNumber;
+    result.innerText = `width :${width.value}ft * breadth :${breadth.value}ft = ${area}sqft`;
+    width.value = breadth.value = null;
+  }
 };
 calculate.onclick = CalcArea;
 
 clear.onclick = clearResult;
 store.onclick = () => {
-  records.innerHTML += `<li> ${result.innerHTML} </li>`;
-  clearResult();
+  if (result.innerHTML) {
+    records.innerHTML += `<li> ${result.innerHTML} </li>`;
+    clearResult();
+  }
 };
---
+++ 3-learn-js-basic/jsDomBom/js/bom.js
@@ -0,0 +1,38 @@
+// // console.log(window);
+// // console.log(window.innerWidth);
+// // console.log(window.innerHeight);
+// // console.log(window.outerWidth);
+// // console.log(window.outerHeight);
+
+// // window.addEventListener("scroll", () => console.log(window.scrollY));
+
+// // window.addEventListener("online", () => {
+// //   console.log("U are online");
+// // });
+
+// // window.addEventListener("offline", () => {
+// //   console.log("U are offline");
+// // // });
+// // x = 35;
+// // y = 37;
+
+// const clock = document.querySelector("#clock");
+// const run = () => {
+//   const d = new Date();
+//   clock.innerText = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+// };
+
+// run();
+
+// // setTimeout(run, 5000)
+// // setInterval(run);
+
+// function text(start, stop) {
+//   let i = start;
+//   const run = setInterval(() => {
+//     console.log("hello", ++i);
+//     if (i === stop) {
+//       clearInterval(run);
+//     }
+//   }, 1000);
+// }
