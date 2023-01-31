77203ffd1c0e462e70d2f2843eb813c98edac080
SutPi0154 <sutpi070@gmail.com>
2023-01-31 10:19:49 +0630

    pj

--- 3-learn-js-basic/jsDomBom/js/slide-show.js
+++ 3-learn-js-basic/jsDomBom/js/slide-show.js
@@ -6,14 +6,16 @@ const createSlideShow = document.querySelector("#createSlideShow");
 const createCarousel = (photoList) => {
   const carousel = document.createElement("div");
   const id = "carousel" + Date.now();
+  carousel.id = id;
   carousel.className = "carousel slide";
   let slides = "";
-  let indicators = " ";
+  let indicators = "";
   photoList.forEach((photo, index) => {
     slides += `
     <div class="carousel-item  ${index === 0 && "active"}">
      <img src="${photo}" class="d-block w-50 mx-auto " alt="...">
   </div>`;
+
     indicators += `
     <button type="button" ${
       index === 0 && `class="active"`
@@ -21,18 +23,11 @@ const createCarousel = (photoList) => {
   });
 
   carousel.innerHTML = `
-  <div id="${id}" class="carousel slide">
   <div class="carousel-indicators">
    ${indicators}
   </div>
   <div class="carousel-inner">
    ${slides}
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
-    </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#${id}" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -42,7 +37,6 @@ const createCarousel = (photoList) => {
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
-</div>
   `;
   document.body.append(carousel);
 };
@@ -55,7 +49,6 @@ photoUpload.addEventListener("change", (event) => {
 
     const reader = new FileReader();
     reader.addEventListener("load", (event) => {
-      console.log(event.target);
       img.src = event.target.result;
       console.log(img);
       // document.body.append(img);
@@ -66,6 +59,7 @@ photoUpload.addEventListener("change", (event) => {
     reader.readAsDataURL(file);
   });
 });
+
 uploadUI.addEventListener("click", () => {
   photoUpload.click();
 });
